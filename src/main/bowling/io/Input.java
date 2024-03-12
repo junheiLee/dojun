ea887edff6faf140d97f755c86ff579b73f9bc17
junheiLee <jun2ee22@naver.com>
2024-03-12 16:14:29 +0900

    Feat: 쓰러뜨린 핀 개수를 입력하는 기능<br> &nbsp; 0과 10 사이의 수를 입력하면 다시 입력해야햔다. 숫자가 아닐 경우 다시 입력해야 한다.

--- src/main/bowling/io/Input.java
+++ src/main/bowling/io/Input.java
@@ -6,11 +6,18 @@ import java.util.Scanner;
 public class Input {
 
     private final static String GUIDE_PARTICIPANT_INPUT = "참여 인원을 입력하세요 : ";
+    private final static String GUIDE_KNOCKED_PIN_INPUT = "쓰러뜨린 볼링 핀 개수를 입력하세요 : ";
     private final static String GUIDE_CORRECT_INPUT = "숫자로 입력하세요.";
+    private final static String GUIDE_VALIDATION_PIN = "0이상 10이하만 가능합니다.";
 
     public int receiveParticipants() {
         System.out.println(GUIDE_PARTICIPANT_INPUT);
         return receive();
+    }
+
+    public int receiveKnockedPin() {
+        System.out.println(GUIDE_KNOCKED_PIN_INPUT);
+        return validPinNum(receive());
     }
 
     private int receive() {
@@ -26,5 +33,13 @@ public class Input {
         }
 
         return number;
+    }
+
+    private int validPinNum(int num) {
+        if (num < 0 || 10 < num) {
+            System.out.println(GUIDE_VALIDATION_PIN);
+            return validPinNum(receive());
+        }
+        return num;
     }
 }
